7db15237625bca4e5277de7e032d5b949ff2bb2e
SomewhereOutInSpace <jameshaywood823@gmail.com>
2022-03-25 23:17:49 -0400

    Started work on path traversal

--- src/Autonomous/PathManagement.h
+++ src/Autonomous/PathManagement.h
@@ -1,8 +1,58 @@
 #pragma once
 #include "PathComponents.h"
+#include "InertialOdometry.h"
 #include "PathCalculation.h"
+#include "PathTraversal.h"
+
+struct TraversalParameters
+{
+    double lookahead_distance;
+};
+
+struct TraversalCache
+{
+    TraversalParameters params;
+    RobotProperties robot_properties;
+    RobotPosition current_position;
+    Path path;
+    PathPoint closest_point;
+    PathPoint lookahead_point;
+    int closest_index;
+    int lookahead_index;
+};
+
+class PathTraverser
+{
+    private:
+
+        TraversalCache cache;
+
+    public:
+
+        PathTraverser(Path path_to_traverse, TraversalParameters parameters, RobotProperties robot)
+        {
+            cache.path = path_to_traverse;
+            cache.params = parameters;
+            cache.robot_properties = robot;
+        }
+
+        void traversePath()
+        {
+            //fuckign GO
+        }
+};
 
 class PathManager
 {
+    private:
 
+        RobotProperties robot_props;
+        std::vector<Path> stored_paths;
+
+    public:
+
+        PathManager(RobotProperties properties)
+        {
+            robot_props = properties;
+        }
 };
--- src/Autonomous/PathTraversal.h
+++ src/Autonomous/PathTraversal.h
@@ -1,2 +1,30 @@
 #pragma once
-#include "PathManagement.h"
+
+double rateLimit()
+{
+    return 0.0;
+}
+
+void updateClosestPoint(TraversalCache& cache)
+{
+    double curr_closest_distance = DBL_MAX;
+    int curr_closest_index = cache.closest_index;
+
+    for(int i = curr_closest_index; i < cache.path.size(); i++)
+    {
+        double distance = interpointDistance(cache.current_position, cache.path.at(i));
+        if(distance < curr_closest_distance)
+        {
+            curr_closest_distance = distance;
+            curr_closest_index = i;
+        }
+    }
+
+    cache.closest_index = curr_closest_index;
+    cache.closest_point = cache.path.at(curr_closest_index);
+}
+
+void updateLookaheadPoint(TraversalCache& cache)
+{
+
+}
