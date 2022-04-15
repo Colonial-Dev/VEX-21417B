32e18a34ef2419305dc7e0b750017ad42bbe4b60
SomewhereOutInSpace <jameshaywood823@gmail.com>
2022-04-15 13:07:02 -0400

    Updated bot_push

--- bot_push.sh
+++ bot_push.sh
@@ -7,8 +7,8 @@ GREEN='\e[1;32m'
 pros make
 
 echo -e "${BLUE}RSyncing hot package to Pi...${NC}"
-sshpass -p "sudoking" rsync ~/Documents/21417/bin/hot.* eagle@cubesat:/home/eagle/hot_package
+sshpass -p "robokauz" rsync -P ~/Documents/21417/bin/hot.package.* eagle@cubesat.local:/home/eagle/program_binary
 echo -e "${GREEN}Done.${NC}"
 echo -e "${BLUE}Uploading hot package to robot...${YELLOW}" 
-sshpass -p "sudoking" ssh eagle@cubesat 'bash bot_upload.sh'
+sshpass -p "robokauz" ssh eagle@cubesat.local 'cd ~/program_binary && ~/.local/bin/pros upload hot.package.* --slot 1 --target v5'
 echo -e "${GREEN}Done.${NC}"
